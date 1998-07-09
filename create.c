14e119d3c0ff2443112bca685ab52f1ad7f8fe8c
bje <bje>
1998-07-09 13:14:50 +0000

    1998-07-09  Ben Elliston  <bje@cygnus.com>
            * create.c (pthread_create): A dummy stub right now.

---
+++ create.c
@@ -0,0 +1,17 @@
+/*
+ * create.c
+ *
+ * Description:
+ * This translation unit implements routines associated with spawning a new
+ * thread.
+ */
+
+#include "pthread.h"
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+		   void * (*start_routine) (void *), void * arg)
+{
+  /* Call Win32 CreateThread.
+     Map attributes as correctly as possible.
+  */
+}
