e21fa2dccba8298c717364f536ac87c50f839a53
bje <bje>
1998-07-10 14:25:21 +0000

    1998-07-11  Ben Elliston  <bje@cygnus.com>
    	* misc.c (pthread_equal): Implement.

--- misc.c
+++ misc.c
@@ -7,7 +7,8 @@
 
 #include "pthread.h"
 
-pthread_t pthread_self(void)
+pthread_t
+pthread_self(void)
 {
   /* It looks like a pthread_t needs to be a HANDLE, but Win32 also has
      another way of identifying threads: their thread id.  We hope
@@ -15,4 +16,14 @@ pthread_t pthread_self(void)
      HANDLEs.  The morons. */
 
   return GetCurrentThread();
+}
+
+int
+pthread_equal(pthread_t t1, pthread_t t2)
+{
+  /* For the time being, assume that HANDLEs can be directly compared.
+     If not, then use the appropriate Win32 function for
+     comparison. */
+
+  return (t1 != t2);
 }
