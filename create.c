6e1572fd412dbe05ba73b5bf4a1c9a3716fb026a
bje <bje>
1998-07-10 14:22:56 +0000

    * create.c: Tidy function signature.

--- create.c
+++ create.c
@@ -8,8 +8,9 @@
 
 #include "pthread.h"
 
-int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-		   void * (*start_routine) (void *), void * arg)
+int
+pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+	       void * (*start_routine) (void *), void * arg)
 {
   /* Call Win32 CreateThread.
      Map attributes as correctly as possible.
