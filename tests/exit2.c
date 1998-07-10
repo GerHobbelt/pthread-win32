dd0d3032087734ae660fba5d2218c9458eb7a390
bje <bje>
1998-07-10 13:06:05 +0000

    1998-07-10  Ben Elliston  <bje@cygnus.com>
            * exit2.c: New file; test pthread_exit() harder.

---
+++ tests/exit2.c
@@ -0,0 +1,32 @@
+/*
+ * Test for pthread_exit().
+ *
+ * Depends on API functions: pthread_create().
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+
+void *
+func(void * arg)
+{
+	printf("Hello world\n");
+	pthread_exit(arg);
+}
+
+int
+main(int argc, char * argv[])
+{
+	pthread_t id[2];
+	int i;
+
+	/* Create a few threads, make them say hello and then exit. */
+
+	for (i = 0; i < 4; i++)
+	{
+		pthread_create(&id, NULL, entry, (void *) i);
+	}
+
+	/* Semantics should be the same as POSIX. Wait for the workers. */
+	pthread_exit((void *) 0);
+}
