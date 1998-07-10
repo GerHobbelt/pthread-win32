13bca995cb79caa89f6072a8031f8506257bc3ea
bje <bje>
1998-07-10 12:45:08 +0000

    1998-07-10  Ben Elliston  <bje@cygnus.com>
            * exit.c (pthread_exit): Implement.

---
+++ exit.c
@@ -0,0 +1,19 @@
+/*
+ * exit.c
+ *
+ * Description:
+ * This translation unit implements routines associated with exiting from
+ * a thread.
+ */
+
+#include "pthread.h"
+
+void pthread_exit(void * value)
+{
+  /* The semantics are such that additional tasks must be done for
+     strict POSIX conformance.  We must add code here later which 
+     deals with executing cleanup handlers and such.  For now, the
+     following is mostly correct: */
+
+  ExitThread((DWORD) value);
+}
