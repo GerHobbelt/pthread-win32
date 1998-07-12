aaed4badccb03dc0a7b527f437df54f992e2d988
bje <bje>
1998-07-12 14:57:13 +0000

    1998-07-13  Ben Elliston  <bje@cygnus.com>
    	* implement.h (PTHREADS_THREADS_MAX): Remove trailing semicolon.
    	(PTHREADS_STACK_MIN): Specify; needs confirming.
    	(_pthreads_attr_t): Define this type.
    	(_pthreads_condattr_t): Likewise.

--- implement.h
+++ implement.h
@@ -5,13 +5,21 @@
  * by other parts of this pthreads implementation internally.
  */
 
-/* FIXME: Arbitrary. Need value from Win32.
+/* FIXME: Arbitrary. Need values from Win32.
  */
-#define PTHREAD_THREADS_MAX 256;
+#define PTHREAD_THREADS_MAX 256
+#define PTHREAD_STACK_MIN 65535
 
 extern DWORD pthreads_thread_count;
 
 typedef struct {
-  int proc_shared;
+  size_t stacksize;
+} _pthread_attr_t;
+
+typedef struct {
+  int pshared;
 } _pthread_mutexattr_t;
 
+typedef struct {
+  int pshared;
+} _pthread_condattr_t;
