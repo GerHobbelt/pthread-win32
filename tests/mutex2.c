949ca2ccc5c972644c70238eee7d2099a3ede1f3
rpj <rpj>
1999-02-20 03:06:22 +0000

    1999-02-20  Ross Johnson  <rpj@ise.canberra.edu.au>         * tests/mutex2.c: Test static mutex initialisation.         * tests/test.h: New. Declares a table mapping error numbers to         error names.

--- tests/mutex2.c
+++ tests/mutex2.c
@@ -1,12 +1,25 @@
+/* 
+ * mutex1.c
+ *
+ * Declare a static mutex object, lock it, and then unlock it again.
+ */
+
 #include <pthread.h>
+#include "test.h"
  
-pthread_mutex_t mutex1;
- 
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+
 main()
 {
-  pthread_mutex_init(&mutex1, NULL);
-  pthread_mutex_trylock(&mutex1);
-  pthread_mutex_unlock(&mutex1);
+  int result;
+
+  result = pthread_mutex_trylock(&mutex1);
+  printf("pthread_mutex_trylock returned %s\n", error_string[result]);
+
+  if (result == 0)
+  {
+    pthread_mutex_unlock(&mutex1);
+  }
 
   return 0;
 }
---
+++ tests/test.h
@@ -0,0 +1,58 @@
+/* 
+ * test.h
+ *
+ * Useful definitions and declarations for tests.
+ */
+
+#include <pthread.h>
+
+#ifndef _PTHREAD_TEST_H_
+#define _PTHREAD_TEST_H_
+
+char * error_string[] = {
+  "ZERO",
+  "EPERM",
+  "ENOFILE_or_ENOENT",
+  "ESRCH",
+  "EINTR",
+  "EIO",
+  "ENXIO",
+  "E2BIG",
+  "ENOEXEC",
+  "EBADF",
+  "ECHILD",
+  "EAGAIN",
+  "ENOMEM",
+  "EACCES",
+  "EFAULT",
+  "UNKNOWN_15",
+  "EBUSY",
+  "EEXIST",
+  "EXDEV",
+  "ENODEV",
+  "ENOTDIR",
+  "EISDIR",
+  "EINVAL",
+  "ENFILE",
+  "EMFILE",
+  "ENOTTY",
+  "UNKNOWN_26",
+  "EFBIG",
+  "ENOSPC",
+  "ESPIPE",
+  "EROFS",
+  "EMLINK",
+  "EPIPE",
+  "EDOM",
+  "ERANGE",
+  "UNKNOWN_35",
+  "EDEADLOCK_or_EDEADLK",
+  "UNKNOWN_37",
+  "ENAMETOOLONG",
+  "ENOLCK",
+  "ENOSYS",
+  "ENOTEMPTY",
+  "EILSEQ",
+};
+
+#endif
