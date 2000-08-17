88de1621e4371cd118e1a98c9c08ff23c46e6899
rpj <rpj>
2000-08-17 14:50:43 +0000

    Removed obsolete files.

--- tests/ccl.bat
+++
@@ -1,9 +0,0 @@
-REM Generate preprocessor output
-REM cl /E /W3 /MT /nologo /Yd /Zi -I. -D_WIN32_WINNT=0x400 -DSTDCALL=_stdcall -c ..\%1.c > ..\%1.e
-
-REM Generate object file
-cl /W3 /MT /nologo /Yd /Zi -I. -D_WIN32_WINNT=0x400 -DSTDCALL=_stdcall -c ..\%1.c
-
-REM Generate executable
-cl /nologo /Feaout.exe /Zi %1.obj .\pthread.lib
-del %1.obj > nul:
