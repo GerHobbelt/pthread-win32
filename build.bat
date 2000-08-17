186a157150b299c82401bc222cd34ba310e2a49b
rpj <rpj>
2000-08-17 14:50:43 +0000

    Removed obsolete files.

--- build.bat
+++
@@ -1,4 +0,0 @@
-del aout.exe
-del %1.obj
-cl /W3 /MT /nologo /Yd /Zi -I. -D_WIN32_WINNT=0x400 -DSTDCALL=_stdcall -c %1.c
-cl /Feaout.exe /Zi %1.obj pthread.lib
--- tests/cgcc.bat
+++
@@ -1,1 +0,0 @@
-gcc -o aout.exe ..\%1.c -I. -L. -lpthread32
