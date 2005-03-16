57c6a64c057b2d6108a3af55a7ad69b83175b56f
rpj <rpj>
2005-03-16 01:36:11 +0000

    ''

--- tests/mutex5.c
+++ tests/mutex5.c
@@ -41,19 +41,23 @@
 
 static pthread_mutexattr_t mxAttr;
 
+/* Prevent optimiser from removing dead or obvious asserts. */
+int _optimiseFoil;
+#define FOIL(x) (_optimiseFoil = x)
+
 int
 main()
 {
   int mxType = -1;
 
-  assert(PTHREAD_MUTEX_DEFAULT == PTHREAD_MUTEX_NORMAL);
-  assert(PTHREAD_MUTEX_DEFAULT != PTHREAD_MUTEX_ERRORCHECK);
-  assert(PTHREAD_MUTEX_DEFAULT != PTHREAD_MUTEX_RECURSIVE);
-  assert(PTHREAD_MUTEX_RECURSIVE != PTHREAD_MUTEX_ERRORCHECK);
+  assert(FOIL(PTHREAD_MUTEX_DEFAULT) == PTHREAD_MUTEX_NORMAL);
+  assert(FOIL(PTHREAD_MUTEX_DEFAULT) != PTHREAD_MUTEX_ERRORCHECK);
+  assert(FOIL(PTHREAD_MUTEX_DEFAULT) != PTHREAD_MUTEX_RECURSIVE);
+  assert(FOIL(PTHREAD_MUTEX_RECURSIVE) != PTHREAD_MUTEX_ERRORCHECK);
 
-  assert(PTHREAD_MUTEX_NORMAL == PTHREAD_MUTEX_FAST_NP);
-  assert(PTHREAD_MUTEX_RECURSIVE == PTHREAD_MUTEX_RECURSIVE_NP);
-  assert(PTHREAD_MUTEX_ERRORCHECK == PTHREAD_MUTEX_ERRORCHECK_NP);
+  assert(FOIL(PTHREAD_MUTEX_NORMAL) == PTHREAD_MUTEX_FAST_NP);
+  assert(FOIL(PTHREAD_MUTEX_RECURSIVE) == PTHREAD_MUTEX_RECURSIVE_NP);
+  assert(FOIL(PTHREAD_MUTEX_ERRORCHECK) == PTHREAD_MUTEX_ERRORCHECK_NP);
 
   assert(pthread_mutexattr_init(&mxAttr) == 0);
   assert(pthread_mutexattr_gettype(&mxAttr, &mxType) == 0);
