432aa623cd5d17b65f73d34a1873a7ece777f63b
Ross Johnson <ross.johnson@homemail.com.au>
2012-10-29 09:12:17 +1100

    Reduce Sleeps

--- tests/exception1.c
+++ tests/exception1.c
@@ -16,17 +16,17 @@
  *      code distribution. The list can also be seen at the
  *      following World Wide Web location:
  *      http://sources.redhat.com/pthreads-win32/contributors.html
- * 
+ *
  *      This library is free software; you can redistribute it and/or
  *      modify it under the terms of the GNU Lesser General Public
  *      License as published by the Free Software Foundation; either
  *      version 2 of the License, or (at your option) any later version.
- * 
+ *
  *      This library is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *      Lesser General Public License for more details.
- * 
+ *
  *      You should have received a copy of the GNU Lesser General Public
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
@@ -37,22 +37,22 @@
  * Test Synopsis: Test passing of exceptions back to the application.
  *
  * Test Method (Validation or Falsification):
- * - 
+ * -
  *
  * Requirements Tested:
  * -
  *
  * Features Tested:
- * - 
+ * -
  *
  * Cases Tested:
- * - 
+ * -
  *
  * Description:
- * - 
+ * -
  *
  * Environment:
- * - 
+ * -
  *
  * Input:
  * - None.
@@ -208,7 +208,7 @@ main()
   /*
    * Code to control or manipulate child threads should probably go here.
    */
-  Sleep(1000);
+  Sleep(100);
 
   for (i = 0; i < NUMTHREADS; i++)
     {
@@ -218,7 +218,7 @@ main()
   /*
    * Give threads time to run.
    */
-  Sleep(NUMTHREADS * 1000);
+  Sleep(NUMTHREADS * 100);
 
   /*
    * Check any results here. Set "failed" and only print output on failure.
