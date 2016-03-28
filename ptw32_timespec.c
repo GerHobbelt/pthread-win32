8ddeaf15bc5bea6d665cceee231947bf390a726d
rpj <Ross.Johnson@loungebythelake.net>
2016-03-28 14:02:18 +1100

    Whole of content dependent on NEED_FTIME config switch.

--- ptw32_timespec.c
+++ ptw32_timespec.c
@@ -40,6 +40,8 @@
 # include <config.h>
 #endif
 
+#if defined(NEED_FTIME)
+
 #include "pthread.h"
 #include "implement.h"
 
@@ -81,3 +83,5 @@ ptw32_filetime_to_timespec (const FILETIME * ft, struct timespec *ts)
     (int) ((*(uint64_t *) ft - PTW32_TIMESPEC_TO_FILETIME_OFFSET -
 	    ((uint64_t) ts->tv_sec * (uint64_t) 10000000UL)) * 100);
 }
+
+#endif
