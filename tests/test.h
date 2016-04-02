c41614783a489a5986f0b65e9d97cc7e0136f2d3
rpj <Ross.Johnson@loungebythelake.net>
2016-04-02 17:43:46 +1100

    Fix a basic race. Fix a type comparison error in assert_e.

--- tests/test.h
+++ tests/test.h
@@ -163,7 +163,7 @@ int assertE;
 			            #e, __FILE__, (int) __LINE__), \
 	                            fflush(stderr) : \
                              0) : \
-       (assertE <= sizeof(error_string)/sizeof(error_string[0])) ? \
+       (assertE <= (int) (sizeof(error_string)/sizeof(error_string[0]))) ? \
 	   (fprintf(stderr, "Assertion failed: (%s %s %s), file %s, line %d, error %s\n", \
 			    #e,#o,#r, __FILE__, (int) __LINE__, error_string[assertE]), exit(1), 0) :\
 		   (fprintf(stderr, \
