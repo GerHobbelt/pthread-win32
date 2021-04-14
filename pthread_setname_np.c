7421f691f5d93e7e692cbfd381a54771a28a9fe5
Ger Hobbelt <ger@hobbelt.com>
2021-04-14 11:37:21 +0200

    more banner work for pre-merge

--- pthread_setname_np.c
+++ pthread_setname_np.c
@@ -30,6 +30,8 @@
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
  *      59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *
+ * --------------------------------------------------------------------------
  */
 
 #ifdef HAVE_CONFIG_H
