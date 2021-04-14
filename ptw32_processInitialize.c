b944fa9e6c532a60c147c6a0f01a50e534fc1404
Ger Hobbelt <ger@hobbelt.com>
2021-04-14 14:50:47 +0200

    Revert "init all variables from global.c in global.c", at least the 'global vars init' part: re-init those vars when the lib is cleaned up & re-initialized (i.e. in test rigs)
    This reverts commit e70d6a53b87511204352346d12dcf6c21a1342c3.

--- ptw32_processInitialize.c
+++ ptw32_processInitialize.c
@@ -76,9 +76,73 @@ ptw32_processInitialize (void)
     }
 
   /*
-   * All variables from global.c are initialized in global.c !
-   * ptw32_processTerminate() could return global variables values to initial state if needed
+   * Explicitly initialise all variables from global.c
    */
+  ptw32_threadReuseTop = PTW32_THREAD_REUSE_EMPTY;
+  ptw32_threadReuseBottom = PTW32_THREAD_REUSE_EMPTY;
+  ptw32_selfThreadKey = NULL;
+  ptw32_cleanupKey = NULL;
+  ptw32_cond_list_head = NULL;
+  ptw32_cond_list_tail = NULL;
+
+  ptw32_concurrency = 0;
+
+  /* What features have been auto-detected */
+  ptw32_features = 0;
+
+  /*
+   * Global [process wide] thread sequence Number
+   */
+  ptw32_threadSeqNumber = 0;
+
+  /*
+   * Function pointer to QueueUserAPCEx if it exists, otherwise
+   * it will be set at runtime to a substitute routine which cannot unblock
+   * blocked threads.
+   */
+  ptw32_register_cancellation = NULL;
+
+  /*
+   * Global lock for managing pthread_t struct reuse.
+   */
+  ptw32_thread_reuse_lock = 0;
+
+  /*
+   * Global lock for testing internal state of statically declared mutexes.
+   */
+  ptw32_mutex_test_init_lock = 0;
+
+  /*
+   * Global lock for testing internal state of PTHREAD_COND_INITIALIZER
+   * created condition variables.
+   */
+  ptw32_cond_test_init_lock = 0;
+
+  /*
+   * Global lock for testing internal state of PTHREAD_RWLOCK_INITIALIZER
+   * created read/write locks.
+   */
+  ptw32_rwlock_test_init_lock = 0;
+
+  /*
+   * Global lock for testing internal state of PTHREAD_SPINLOCK_INITIALIZER
+   * created spin locks.
+   */
+  ptw32_spinlock_test_init_lock = 0;
+
+  /*
+   * Global lock for condition variable linked list. The list exists
+   * to wake up CVs when a WM_TIMECHANGE message arrives. See
+   * w32_TimeChangeHandler.c.
+   */
+  ptw32_cond_list_lock = 0;
+
+  #if defined(_UWIN)
+  /*
+   * Keep a count of the number of threads.
+   */
+  pthread_count = 0;
+  #endif
 
   /*
    * Initialize Keys
