ce4684b35e25cf5303b3e8fed23ca4c438e54021
SongZihuan <jimsong2010@163.com>
2021-10-13 17:05:14 +0800

    fix: platform judgment

--- cmake/target_arch.cmake
+++ cmake/target_arch.cmake
@@ -1,19 +1,20 @@
 
 set(TARGET_ARCH_DETECT_CODE "
-
-    #if defined(_M_ARM)
-        #error cmake_arch ARM
-    #elif defined(_M_ARM64)
-        #error cmake_arch ARM64
-    #elif defined(_M_AMD64)
-        #error cmake_arch x86_64
-    #elif defined(_M_X64)
-        #error cmake_arch x64
-    #elif defined(_M_IX86)
-        #error cmake_arch x86
-    #else
-        #error cmake_arch unknown
+    int main() {
+        #if defined(_M_ARM)
+            return 2;
+        #elif defined(_M_ARM64)
+            return 3;
+        #elif defined(_M_AMD64)
+            return 4;
+        #elif defined(_M_X64)
+            return 5;
+        #elif defined(_M_IX86)
+            return 6;
+        #else
+            return 0;
     #endif
+}
 ")
 
 function(get_target_arch out)
@@ -23,14 +24,20 @@ function(get_target_arch out)
         "${TARGET_ARCH_DETECT_CODE}")
 
     try_run(
-        run_result_unused compile_result_unused
-        "${CMAKE_BINARY_DIR}" "${CMAKE_BINARY_DIR}/target_arch_detect.c"
-        COMPILE_OUTPUT_VARIABLE TARGET_ARCH)
+        run_result compile_result_unused
+        "${CMAKE_BINARY_DIR}" "${CMAKE_BINARY_DIR}/target_arch_detect.c")
 
-    # parse compiler output
-    string(REGEX MATCH "cmake_arch ([a-zA-Z0-9_]+)" TARGET_ARCH "${TARGET_ARCH}")
-    string(REPLACE "cmake_arch " "" TARGET_ARCH "${TARGET_ARCH}")
-
-    set(${out} "${TARGET_ARCH}" PARENT_SCOPE)
-
+    if (run_result STREQUAL 2)
+        set(${out} "ARM" PARENT_SCOPE)
+    elseif (run_result STREQUAL 3)
+        set(${out} "ARM64" PARENT_SCOPE)
+    elseif (run_result STREQUAL 4)
+        set(${out} "x86_64" PARENT_SCOPE)
+    elseif (run_result STREQUAL 5)
+        set(${out} "x64" PARENT_SCOPE)
+    elseif (run_result STREQUAL 6)
+        set(${out} "x86" PARENT_SCOPE)
+    else()
+        set(${out} "unknown" PARENT_SCOPE)
+    endif()
 endfunction()
