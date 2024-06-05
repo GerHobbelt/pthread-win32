451aae769de38d957ad2b6ff3c1f914d0d6cc5c8
Lawrence Stubbs <lstubbs@techno.express>
2024-06-04 23:19:31 -0400

    few #ifdef/#endif for tiny c compiler & build file
    - compiles without issues `tcc @build_dll.tcc` or `tcc @build_obj.tcc`
    
    - update _ptw32.h, config.h, pthread_kill.c and pthread_win32_attach_detach_np.c

--- _ptw32.h
+++ _ptw32.h
@@ -19,17 +19,17 @@
  *      code distribution. The list can also be seen at the
  *      following World Wide Web location:
  *      http://sources.redhat.com/pthreads-win32/contributors.html
- * 
+ *
  *      This library is free software; you can redistribute it and/or
  *      modify it under the terms of the GNU Lesser General Public
  *      License as published by the Free Software Foundation; either
  *      version 2 of the License, or (at your option) any later version.
- * 
+ *
  *      This library is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *      Lesser General Public License for more details.
- * 
+ *
  *      You should have received a copy of the GNU Lesser General Public
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
@@ -51,7 +51,7 @@
 #define  PTW32_VERSION 3,0,3,1
 #define  PTW32_VERSION_STRING "3, 0, 3, 1\0"
 
-// skip the rest when running this through the Microsoft Resource Compiler, which is a VERY brittle piece of machinery! -> RC2188 & RC1116 cryptic failures will be your part! 
+// skip the rest when running this through the Microsoft Resource Compiler, which is a VERY brittle piece of machinery! -> RC2188 & RC1116 cryptic failures will be your part!
 #ifndef RC_INVOKED
 
 #if defined(__GNUC__)
@@ -160,8 +160,13 @@
 #  define int64_t LONGLONG
 #  define uint64_t ULONGLONG
 #elif !defined(__MINGW32__)
-     typedef _int64 int64_t;
-     typedef unsigned _int64 uint64_t;
+#if defined(__TINYC__)
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+#else
+typedef _int64 int64_t;
+typedef unsigned _int64 uint64_t;
+#endif
 #  if defined (PTW32_CONFIG_MSVC6)
      typedef long intptr_t;
 #  endif
--- pthread_win32_attach_detach_np.c
+++ pthread_win32_attach_detach_np.c
@@ -18,17 +18,17 @@
  *      code distribution. The list can also be seen at the
  *      following World Wide Web location:
  *      http://sources.redhat.com/pthreads-win32/contributors.html
- * 
+ *
  *      This library is free software; you can redistribute it and/or
  *      modify it under the terms of the GNU Lesser General Public
  *      License as published by the Free Software Foundation; either
  *      version 2 of the License, or (at your option) any later version.
- * 
+ *
  *      This library is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *      Lesser General Public License for more details.
- * 
+ *
  *      You should have received a copy of the GNU Lesser General Public
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
@@ -86,7 +86,7 @@ pthread_win32_process_attach_np ()
 
 #if !defined(WINCE)
   gsd_res = GetSystemDirectory(QuserExDLLPathBuf, QuserExDLLPathBufSize);
-#if defined(__GNUC__) || defined(PTW32_CONFIG_MSVC7)
+#if defined(__GNUC__) || defined(PTW32_CONFIG_MSVC7) || defined(__TINYC__)
   if(gsd_res && gsd_res < QuserExDLLPathBufSize)
   {
     (void) strncat(QuserExDLLPathBuf,
